b0fb4092283192c8b4fd76f443a40660f90b444f
David Zukor <zukord@gmail.com>
2019-02-18 01:34:40 -0500

    Done for tonight, put in another couple arrays. I'll finish it tomorrow.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,14 +1,17 @@
 var startButton = $("<button id=\"start\"></button>").text("Start");
 var restartButton = $("<button id=\"restart\"></button>").text("Start Over?");
+var intervalId;
 var wins = 0;
 var losses = 0;
 var timeouts = 0;
 var rightOrWrong = -1;
+var questionNumber = 0;
 var questions = ["What is the color of Diamonds?", "How high does the Efficiency enchantment go?", "Which of these is a number of rotten flesh a single zombie can drop?", "Can TNT blow up Obsidian?", "Can the Wither blow up Obsidian?"];
 var diamondAnswers = ["Yellow", "Clear", "Blue", "Red"];
 var efficiencyAnswers = ["II", "III", "V", "VI"];
 var rottenAnswers = ["1", "2", "4", "8"]
 var boolAnswers = ["Yes", "No"]
+var answers = [diamondAnswers, efficiencyAnswers, rottenAnswers, boolAnswers, boolAnswers];
 $("#startButton").append(startButton);
 
 $("#restart").click(function() {
@@ -21,8 +24,14 @@ $("div").empty();
 
 function questionStart(){
     clearInterval(intervalId);
-    intervalId = setInterval(countdown, 1000)
+    intervalId = setInterval(countdown, 1000);
+    askQuestion();
 };
+
+function askQuestion(){
+    $("#question").html(questions[questionNumber]);
+    //$("#answers").html(answers[questionNumber]);
+}
 
 function countdown(){
     var seconds = 30;
@@ -42,21 +51,21 @@ function correct(){
     $("div").empty();
     $("rightOrWrong").html("You ran out of time! The correct answer was:");
     $("gif").html("<img src=\"\"></img>");
-    questions++;
+    questionNumber++;
     setTimeout(questionStart, 5000); 
 }
 function wrong(){
     $("div").empty();
     $("rightOrWrong").html("You ran out of time! The correct answer was:");
     $("gif").html("<img src=\"\"></img>");
-    questions++;
+    questionNumber++;
     setTimeout(questionStart, 5000);
 }
 function quizTimeOut(){
     $("div").empty();
     $("rightOrWrong").html("You ran out of time! The correct answer was:");
     $("gif").html("<img src=\"\"></img>");
-    questions++;
+    questionNumber++;
     setTimeout(questionStart, 5000);
 }
 
